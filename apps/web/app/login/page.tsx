fdb8e91df3fd09c1e0781701ff451de31277c533
WisdomIT <wisdomitx@gmail.com>
2025-04-26 16:16:03 +0900

    ✨ feat: Authentication 관련 로직 정리

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -1,4 +1,4 @@
-import { GalleryVerticalEnd } from 'lucide-react';
+import { Bot } from 'lucide-react';
 import Link from 'next/link';
 
 import { LoginForm } from './_components/loginForm';
@@ -9,7 +9,7 @@ export default function LoginPage() {
       <div className="flex w-full max-w-sm flex-col gap-6">
         <Link href="/" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
+            <Bot className="size-4" />
           </div>
           위즈봇
         </Link>
