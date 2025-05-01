d148b1a7be4b86691817a5ab8e55aec46ec06ed3
WisdomIT <wisdomitx@gmail.com>
2025-05-01 23:53:13 +0900

    ✨ feat: 명령어 샘플 구현

--- apps/web/app/(streamer)/streamer/bot/command/page.tsx
+++ apps/web/app/(streamer)/streamer/bot/command/page.tsx
@@ -1,5 +1,31 @@
-import BodyBreadcrumb from '@/components/body-breadcrumb';
+import { columns, Command } from './_components/columns';
+import { DataTable } from './_components/data-table';
 
 export default function Page() {
-  return <div />;
+  const data = [
+    {
+      id: 1,
+      command: 'hello',
+      type: 'echo',
+      usage: <>!hello</>,
+      usageString: '!hello',
+      description: 'This is a hello command',
+      permission: 'VIEWER',
+    },
+    {
+      id: 1,
+      command: 'hello2',
+      type: 'echo',
+      usage: <>!hello2</>,
+      usageString: '!hello2',
+      description: 'This is a hello2 command',
+      permission: 'VIEWER',
+    },
+  ] as Command[];
+
+  return (
+    <div>
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 }
