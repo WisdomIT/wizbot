b62be06b5a049062233b77a37a2a78d78bbc51d4
WisdomIT <wisdomitx@gmail.com>
2025-05-12 03:15:18 +0900

    🐛 fix: refreshToken 핸들링 로직 변경

--- packages/shared/src/lib/accessToken.ts
+++ packages/shared/src/lib/accessToken.ts
@@ -15,39 +15,34 @@ export async function getAccessToken(ctx: Context, userId: number) {
 
   // Check if the access token is expired
   try {
-    const meRequest = await chzzk.user.me(accessToken);
-    if (meRequest.code !== 200) {
-      // Refresh token
-      const refreshTokenRequest = await chzzk.authorization.refreshToken({
-        refreshToken: credential.refreshToken,
-      });
-      if (refreshTokenRequest.code !== 200) {
-        throw new Error('Invalid response from Chzzk on refresh token request');
-      }
-      const {
+    await chzzk.user.me(accessToken);
+  } catch (error) {
+    // Refresh token
+    const refreshTokenRequest = await chzzk.authorization.refreshToken({
+      refreshToken: credential.refreshToken,
+    });
+    if (refreshTokenRequest.code !== 200) {
+      throw new Error('Invalid response from Chzzk on refresh token request');
+    }
+    const {
+      accessToken: accessTokenNew,
+      refreshToken,
+      tokenType,
+      expiresIn,
+    } = refreshTokenRequest.content;
+    await ctx.prisma.oAuthCredential.update({
+      where: {
+        userId,
+      },
+      data: {
         accessToken: accessTokenNew,
         refreshToken,
         tokenType,
-        expiresIn,
-      } = refreshTokenRequest.content;
-      await ctx.prisma.oAuthCredential.update({
-        where: {
-          userId,
-        },
-        data: {
-          accessToken: accessTokenNew,
-          refreshToken,
-          tokenType,
-          expiresIn: new Date(new Date().getTime() + Number(expiresIn) * 1000),
-        },
-      });
+        expiresIn: new Date(new Date().getTime() + Number(expiresIn) * 1000),
+      },
+    });
 
-      accessToken = accessTokenNew;
-    }
-  } catch (error) {
-    if (error instanceof Error) {
-      throw new Error('Invalid response from Chzzk on me request');
-    }
+    accessToken = accessTokenNew;
   }
 
   return accessToken;
