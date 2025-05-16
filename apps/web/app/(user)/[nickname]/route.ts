ed94904e2e6597392ef4a7f60cf1b7ef3d44a67d
WisdomIT <wisdomitx@gmail.com>
2025-05-16 13:04:18 +0900

    ✨ feat: params를 pathname에서 추출하도록 변경

--- apps/web/app/(user)/[nickname]/route.ts
+++ apps/web/app/(user)/[nickname]/route.ts
@@ -2,8 +2,11 @@ import { NextResponse } from 'next/server';
 
 import { getPublicSiteUrl } from '@/app/login/_apis/chzzk';
 
-export async function GET(request: Request, context: { params: { nickname: string } }) {
-  const { nickname } = context.params;
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const pathnameParts = url.pathname.split('/'); // ['', '닉네임']
+  const nickname = decodeURIComponent(pathnameParts[1] || '');
+
   const publicSiteUrl = await getPublicSiteUrl();
   return NextResponse.redirect(`${publicSiteUrl}/${nickname}/command`);
 }
